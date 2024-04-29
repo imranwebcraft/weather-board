fa5428cde431707b3bb0333fc1645bf5cdb7753e
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-04-29 21:36:48 +0600

    Create file and folder according to mindmap

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "roboto":'"Roboto", sans-serif'
+      },
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+				primary: '#B4D429',
+				dark: "#171923",
+				light: "#fff",
+				body: "#1D1E28"
+			},
+    },
   },
   plugins: [],
 }
