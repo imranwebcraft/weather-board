0cc4ba6439403ba0e922f8cfb33d8becc2b5917b
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-05-01 21:44:29 +0600

    toggole favourite functionality added and store data to the localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Page from "./Page";
 import { WeatherContextProvider } from "./provider";
+import FavouriteContextProvider from "./provider/FavouriteContextProvider";
 
 function App() {
 	return (
 		<WeatherContextProvider>
-			<Page />
+			<FavouriteContextProvider>
+				<Page />
+			</FavouriteContextProvider>
 		</WeatherContextProvider>
 	);
 }
--- src/components/Weather/ToggoleFavorite.jsx
+++ src/components/Weather/ToggoleFavorite.jsx
@@ -1,13 +1,46 @@
-import heart from "../../assets/heart.svg";
+import { useContext, useEffect, useState } from "react";
+import HeartIcon from "../../assets/heart.svg";
+import RedHeartIcon from "../../assets/heart-red.svg";
+import { FavouriteContext, WeatherContext } from "../../context";
 
 const ToggoleFavorite = () => {
+	// state
+	const [isFavorite, setIsFavorite] = useState(false);
+
+	// Context
+
+	const { weatherData } = useContext(WeatherContext);
+	const { location, tatitude, longitude } = weatherData;
+
+	const { favourites, addToFavourites, removeFromFavourites } =
+		useContext(FavouriteContext);
+
+	useEffect(() => {
+		const found = favourites.find((f) => f.location === location);
+		setIsFavorite(found);
+	}, []);
+
+	// Event Handlers
+	const handleToggoleFavourite = () => {
+		const found = favourites.find((fav) => fav.location === location);
+		if (!found) {
+			addToFavourites(tatitude, longitude, location);
+		} else {
+			removeFromFavourites(location);
+		}
+		setIsFavorite(!isFavorite);
+	};
+
 	return (
 		<div className="md:col-span-2">
 			<div className="flex items-center justify-end space-x-6">
-				<button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+				<button
+					onClick={handleToggoleFavourite}
+					className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D] hover:bg-[#a1a1a14d] duration-150"
+				>
 					<span>Add to Favourite</span>
 					<img
-						src={heart}
+						src={isFavorite ? RedHeartIcon : HeartIcon}
 						alt="heart-icon"
 					/>
 				</button>
