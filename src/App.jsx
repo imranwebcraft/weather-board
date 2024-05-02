4355a29b32c880424497d3eacd7ac333af0ae61f
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-05-02 16:36:53 +0600

    get search term from input field and getLocation details according to search text and setSearch term to the search context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Page from "./Page";
-import { WeatherContextProvider } from "./provider";
+import { SearchContextProvider, WeatherContextProvider } from "./provider";
 import FavouriteContextProvider from "./provider/FavouriteContextProvider";
 
 function App() {
 	return (
 		<WeatherContextProvider>
 			<FavouriteContextProvider>
-				<Page />
+				<SearchContextProvider>
+					<Page />
+				</SearchContextProvider>
 			</FavouriteContextProvider>
 		</WeatherContextProvider>
 	);
--- src/components/Header/SearchLocation.jsx
+++ src/components/Header/SearchLocation.jsx
@@ -1,13 +1,31 @@
+import { useContext, useState } from "react";
 import search from "../../assets/search.svg";
+import { SearchContext } from "../../context";
+import { getLocationByName } from "../../data/location-data";
 
 const SearchLocation = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const { setSelectedLocation } = useContext(SearchContext);
+
+	const handleSearch = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const fetchedLocation = getLocationByName(searchTerm);
+		setSelectedLocation({ ...fetchedLocation });
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
 				<input
 					className="bg-transparent placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
 					type="search"
 					placeholder="Search Location"
+					value={searchTerm}
+					onChange={handleSearch}
 					required
 				/>
 				<button type="submit">
